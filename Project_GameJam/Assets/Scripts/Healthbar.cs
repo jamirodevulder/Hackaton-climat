0d57ef48b08126b70bec9f1bb67ce52ee33cb0ed
jamirodevulder <31829657+jamirodevulder@users.noreply.github.com>
2019-02-11 09:39:50 +0100

    TowerDefence
    first build of the towerdefence

---
+++ Project_GameJam/Assets/Scripts/Healthbar.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Healthbar : MonoBehaviour
+{
+    private float Hp;
+    public Text Health;
+
+    void Start()
+    {
+        Hp = 200;
+        HealthText();
+    }
+
+    void Update()
+    {
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Hp > 0)
+            {
+                TakeDamage();
+            }
+            else
+            {
+                print("Dead");
+            }
+        }
+    }
+
+    void TakeDamage()
+    {
+        
+        Hp -= 5;
+        HealthText();
+    }
+
+    void HealthText()
+    {
+        Health.text = Hp.ToString();
+    }
+
+
+}
---
+++ Project_GameJam/Assets/Scripts/Shoot.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+public class Shoot : MonoBehaviour
+{
+
+    public GameObject kogel;
+    public GameObject spawn;
+    public GameObject radius;
+    public bool inRadius = false;
+    public bool schiet = false;
+    public string welkeTegenstander;
+    public string test;
+    public float speed;
+    public int geraakt;
+    private int teller;
+    private int checker = 1;
+    private string[] array = new string[50];
+    void Start()
+    {
+        
+        InvokeRepeating("Schiet", 2.0f, 5.0f);
+
+    }
+    void Update()
+    {
+
+        
+        
+
+
+    }
+    void OnTriggerEnter(Collider collision)
+    {
+        print("test");
+        if (collision.gameObject.tag == "tegenstander")
+        {
+            if (array[teller] == collision.gameObject.name)
+            {
+                
+                
+            }
+            else
+            {
+                teller++;
+                array[teller] = collision.gameObject.name;
+               
+            }
+            if (GameObject.Find(array[checker]) != null)
+             {
+                
+                inRadius = true;
+                welkeTegenstander = array[checker];    
+                print(array[checker]);
+                
+            }
+            else
+            {
+
+                checker++;
+
+            }
+
+        }
+
+
+        Schiet();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        checker++;
+    }
+
+    public void Schiet()
+    {
+        schiet = true;
+
+
+    }
+
+}
